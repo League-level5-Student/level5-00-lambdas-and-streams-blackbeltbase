6e7d7f2564b778a5e3f5cf193573e5cb05b7e307
blackbeltbase <54255614+blackbeltbase@users.noreply.github.com>
2020-10-05 16:57:25 -0700

    lambda

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -1,11 +1,15 @@
 package _00_JButtons_with_Lambdas;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
-public class LambdaButtons {
+public class LambdaButtons  {
 	private JFrame window = new JFrame();
 	private JButton addNumbers = new JButton("ADD 2 Numbers");
 	private JButton randNumber = new JButton("RANDOM NUMBER");
@@ -20,7 +24,9 @@ public class LambdaButtons {
 		
 		//1. Call the addActionListener methods for each button. Use lambdas
 		//   to define to functionality of the buttons.
-		
+		addNumbers.addActionListener(e->{String nums = JOptionPane.showInputDialog("Enter two number seperated by a space. (e.i. 5 6)"); String[] numsNum = nums.split(" "); int num1 = Integer.parseInt(numsNum[0]); int num2 = Integer.parseInt(numsNum[1]); JOptionPane.showMessageDialog(null, numsNum[0] +" plus (+) "+numsNum[1]+ " equals (=) "+(num1+ num2));});
+		randNumber.addActionListener(e->{Random ran = new Random(); JOptionPane.showMessageDialog(null, "Generating random number..."); JOptionPane.showMessageDialog(null, "Generated! Your random number is "+ ran.nextInt());});
+		tellAJoke.addActionListener(e->{JOptionPane.showMessageDialog(null, "What do you call a black screen with your face?"); JOptionPane.showMessageDialog(null, "A mirror with a goblin in it!"); JOptionPane.showMessageDialog(null, "*insert laugh track*");});
 		window.setVisible(true);
 		window.pack();
 	}
@@ -28,6 +34,8 @@ public class LambdaButtons {
 	public static void main(String[] args) {
 		new LambdaButtons();
 	}
+
+
 	
 	
 }
--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -1,9 +1,11 @@
 package _01_Lambda_Methods;
 
+import java.util.Random;
+
 public class LambdaMethods {
 	public static void main(String[] args) {
 		// 1. Look at the SpecialPrinter function interface.
-	
+		
 		// Here is an example of calling the printCustomMessage method with a lambda.
 		// This prints the passed in String 10 times.
 		printCustomMessage((s)->{
@@ -13,13 +15,51 @@ public class LambdaMethods {
 		}, "repeat");
 		
 		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
-		
+		printCustomMessage((s)-> {
+			for(int i = s.length()-1; i>=0;i--) {
+				System.out.print(s.charAt(i));
+			}
+			System.out.println("");
+		}, "backwards");
 		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
-		
+		printCustomMessage((s)-> {
+			Random ran = new Random();
+			for(int i = 0; i<s.length();i++) {
+				String letter = ""+s.charAt(i);
+				if(ran.nextInt()%2 == 0) {
+					letter = letter.toUpperCase();
+				}
+				else {
+					letter = letter.toLowerCase();
+				}
+				System.out.print(letter);
+			}
+			System.out.println("");
+		}, "mixed case");
 		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
-		
+		printCustomMessage((s)-> {
+			for(int i = 0; i<s.length();i++) {
+				String letter = s.charAt(i)+" . ";
+				System.out.print(letter);
+			}
+			System.out.println("");
+		}, "period in between");
 		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
-	
+		printCustomMessage((s)-> {
+			for(int i = 0; i<s.length();i++) {
+				String letter = s.charAt(i)+"";
+				switch(letter) {
+				case "a": break;
+				case "e":break;
+				case "i":break;
+				case "o":break;
+				case "u":break;
+				default: System.out.print(letter);
+				break;
+				}
+			}
+			System.out.println("");
+		}, "consanants only");
 	}
 	
 	public static void printCustomMessage(SpecialPrinter sp, String value) {
